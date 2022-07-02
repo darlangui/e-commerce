f40ada14da8a44a66d2a89ff826c93f7eff91b03
guilhermeulbriki <guilhermeulbriki@gmail.com>
2022-07-02 15:25:40 -0300

    Finish home page

---
+++ utils/handleProfileDropdown.js
@@ -0,0 +1,24 @@
+function listenProfileClick() {
+  const profile = document.getElementById('profile');
+  const dropdown = document.getElementById('dropdown');
+
+  profile.addEventListener(
+    'click',
+    () => {
+      console.log('teste');
+
+      if (dropdown.classList.contains('open')) {
+        dropdown.classList.remove('open');
+        dropdown.classList.add('close');
+        dropdown.style.display = 'none';
+      } else {
+        dropdown.classList.remove('close');
+        dropdown.classList.add('open');
+        dropdown.style.display = 'block';
+      }
+    },
+    false
+  );
+}
+
+document.addEventListener('DOMContentLoaded', listenProfileClick, false);
